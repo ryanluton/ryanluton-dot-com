9ae419f05fb38136b9eba530215f47812d5c3c3c
Ryan Luton <ryan@lutonmedia.com>
2014-11-10 16:25:37 -0600

    gulpfile build change
    updated gulp to push directories not previously included in dist. i.e. downloads
    
    Signed-off-by: Ryan Luton <ryan@lutonmedia.com>

--- gulpfile.js
+++ gulpfile.js
@@ -54,6 +54,7 @@ gulp.task('fonts', function () {
 gulp.task('extras', function () {
   return gulp.src([
     'app/*.*',
+    'app/*/*.*',
     '!app/*.html',
     'node_modules/apache-server-configs/dist/.htaccess'
   ], {
